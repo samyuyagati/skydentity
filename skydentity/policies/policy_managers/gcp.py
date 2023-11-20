d5383cde3e926ec6407f8c20c6e7d7626aba0e1b
Karthik Dharmarajan <karthikdharmarajan@berkeley.edu>
2023-11-20 14:27:00 -0800

    Moved over the GCP Policy Manager

---
+++ skydentity/policies/policy_managers/gcp.py
@@ -0,0 +1,48 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+from skydentity.policies.policy_managers.policy_manager import PolicyManager
+from skydentity.policies.checker.policy import Policy
+
+class GCPPolicyManager(PolicyManager):
+    """
+    A policy manager for GCP.
+    """
+
+    def __init__(self, 
+                 credentials_path: str,
+                 firestore_policy_collection: str = 'policies'):
+        """
+        Initializes the GCP policy manager.
+        :param credentials_path: The path to the credentials file.
+        """
+        self._cred = credentials.Certificate(credentials_path)
+        self._app = firebase_admin.initialize_app(self._cred)
+        self._db = firestore.client()
+        self._firestore_policy_collection = firestore_policy_collection
+
+    def upload_policy(self, public_key: str, policy: Policy):
+        """
+        Uploads a policy to GCP.
+        :param public_key: The public key of the policy.
+        :param policy: The policy to upload.
+        """
+        self._db \
+            .collection(self._firestore_policy_collection) \
+            .document(public_key) \
+            .set(policy.to_dict())
+
+    def get_policy(self, public_key: str) -> Policy:
+        """
+        Gets a policy from the cloud vendor.
+        :param public_key: The public key of the policy.
+        :return: The policy.
+        """
+        return Policy.from_dict(
+            self._db \
+                .collection(self._firestore_policy_collection) \
+                .document(public_key) \
+                .get() \
+                .to_dict()
+        )
--- skydentity/policies/policy_managers/policy_manager.py
+++ skydentity/policies/policy_managers/policy_manager.py
@@ -9,5 +9,15 @@ class PolicyManager(ABC):
     def upload_policy(self, public_key: str, policy: Policy):
         """
         Uploads a policy to the cloud vendor.
+        :param public_key: The public key of the policy.
+        :param policy: The policy to upload.
+        """
+        raise NotImplementedError
+    
+    def get_policy(self, public_key: str) -> Policy:
+        """
+        Gets a policy from the cloud vendor.
+        :param public_key: The public key of the policy.
+        :return: The policy.
         """
         raise NotImplementedError
