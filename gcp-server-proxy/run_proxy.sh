242eff2375c52d590b05f66ab5f818fd201bc532
Alec Li <smartspot2@gmail.com>
2024-01-26 10:45:52 -0800

    Fix policy checking initialization, refactor proxy configuration setup (#12)

--- gcp-server-proxy/run_proxy.sh
+++ gcp-server-proxy/run_proxy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+#
+export CERT_FILE="certs/domain_dir/domain.crt"
+export CERT_KEY="certs/domain_dir/domain.key"
 
-flask run --host="0.0.0.0" --port=5000 --cert=certs/domain_dir/domain.crt --key=certs/domain_dir/domain.key
+flask run --host="0.0.0.0" --port=5000 --cert=$CERT_FILE --key=$CERT_KEY
