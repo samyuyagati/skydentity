7d336dd278cf8e46f7983c2ea4e940beacba3e44
Samyukta Yagati <samyu@berkeley.edu>
2024-02-13 10:50:28 -0800

    test_authorizations works for serverless-gcp-skydentity

--- skydentity/scripts/test_authorizations.sh
+++ skydentity/scripts/test_authorizations.sh
@@ -31,39 +31,63 @@ if [ $LOCAL ]; then
     echo "Running local client proxy"
     echo $ROOT
     pushd $ROOT/serverless-gcp-skydentity
-    ./run_proxy_http.sh 
+    ./run_proxy_http.sh &
+    CLIENT_PROCESS=$!
+    echo "Client proxy process: $CLIENT_PROCESS"
     popd
-    SKYID_CLIENT_ADDRESS="http://127.0.0.1:5001/"
+    CLIENT_ADDRESS="http://127.0.0.1:5001/"
 else
     # serverless client proxy
-    $ROOT/serverless-gcp-skydentity/deploy_setup.sh
-    $ROOT/serverless-gcp-skydentity/deploy.sh
-    SKYID_CLIENT_ADDRESS=$(gcloud run services list | grep "https://skyidproxy-service")
+    pushd $ROOT/serverless-gcp-skydentity
+    ./deploy_setup.sh
+    ./deploy.sh
+    CLIENT_ADDRESS=$(gcloud run services list | grep "https://skyidproxy-service")
 fi
 
-echo "Client proxy address: $SKYID_CLIENT_ADDRESS"
+echo "Client proxy address: $CLIENT_ADDRESS"
 pushd $ROOT/gcp-server-proxy
-./run_proxy_http.sh 
+SKYID_CLIENT_ADDRESS=$CLIENT_ADDRESS ./run_proxy_http.sh &
+SERVER_PROCESS=$!
+echo "Server proxy process: $SERVER_PROCESS"
 popd
+
+# Wait for the server and client proxies to start
+echo "Waiting for the server and client proxies to start..."
+sleep 10
 
 # 0b. Upload skydentity/skydentity/policies/config/auth_policy_example.yaml 
 #    to Firestore using upload_policy.py with the --authorization flag
+echo "Uploading auth_policy_example.yaml to Firestore..."
 python upload_policy.py --policy $ROOT/skydentity/policies/config/auth_policy_example.yaml --cloud gcp --public-key skypilot_eval --credentials $CREDS --authorization
 
 # 1. Run send_auth_request.py. In skydentity/skydentity/policies/config/skypilot_eval_with_auth.yaml, 
 # modify the email address of the service account to match the created one.
-python send_auth_request.py --resource_yaml_input="$ROOT/skydentity/policies/config/skypilot_eval.yaml" \
-    --resource_yaml_output="$ROOT/skydentity/policies/config/skypilot_eval_with_auth.yaml" \
+echo "Sending auth request..."
+python send_auth_request.py --resource_yaml_input="$ROOT/skydentity/policies/config/skypilot.yaml" \
+    --resource_yaml_output="$ROOT/skydentity/policies/config/skypilot_eval_with_auth.yaml"\
     --auth_request_yaml="$ROOT/skydentity/policies/config/auth_request_example.yaml" \
     --capability_enc_key="$HOME/.cloud_creds/gcp/proxy-enc/capability_enc.key"
 
 # 2. Upload skypilot_eval_with_auth.yaml to Firestore using upload_policy.py
+echo "Uploading skypilot_eval_with_auth.yaml to Firestore..."
 python upload_policy.py --policy $ROOT/skydentity/policies/config/skypilot_eval_with_auth.yaml --cloud gcp --public-key skypilot_eval --credentials $CREDS
 
 # 3. Run skydentity/python-server/test_server.py.
-python $ROOT/python-server/test_server.py
+echo "Attempting to start VM..."
+pushd $ROOT/python-server
+python test_server.py
+popd
 
 # 4. Check that the service account attached to the created VM matches the one from step 2.
 ATTACHED=$(gcloud compute instances describe gcp-clilib | grep -F3 "serviceAccounts:" | grep "email" | cut -c 10-)
-EXPECTED=$(grep -F1 "authorization:" ../policies/config/skypilot_eval_with_auth.yaml | tail -1 | cut -c 7-)
+EXPECTED=$(grep -F1 "authorization:" $ROOT/skydentity/policies/config/skypilot_eval_with_auth.yaml | tail -1 | cut -c 7-)
 echo "Service account attached to the created VM: $ATTACHED. Expected: $EXPECTED."
+echo "Remember to delete the created VM!"
+
+# Cleanup
+echo "Cleaning up..."
+kill -9 $SERVER_PROCESS
+if [ $LOCAL ]; then
+    kill -9 $CLIENT_PROCESS
+fi
+rm $ROOT/skydentity/policies/config/skypilot_eval_with_auth.yaml
