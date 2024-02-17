29e35a41f404c4d0104e1c5c472e84e323136593
Karthik Dharmarajan <karthikdharmarajan@berkeley.edu>
2024-02-17 02:14:02 -0800

    Added initial azure client proxy

---
+++ azure-client-proxy/proxy_util/policy_check.py
@@ -0,0 +1,62 @@
+"""
+Utility functions for handling policy checking.
+"""
+
+from functools import cache
+from typing import Tuple, Union
+
+from skydentity.policies.managers.azure_authorization_policy_manager import (
+    AzureAuthorizationPolicyManager,
+)
+from skydentity.policies.managers.azure_policy_manager import AzurePolicyManager
+
+from .credentials import get_capability_enc_key, get_service_account_path
+from .logging import get_logger, print_and_log
+
+
+@cache
+def get_policy_manager() -> AzurePolicyManager:
+    service_acct_cred_file = get_service_account_path()
+    # TODO(kdharmarajan): Create this AzurePolicyManager properly
+    return AzurePolicyManager(service_acct_cred_file)
+
+
+@cache
+def get_authorization_policy_manager() -> AzureAuthorizationPolicyManager:
+    service_acct_cred_file = get_service_account_path()
+    capability_enc_key_file = get_capability_enc_key()
+    print("CREATING AUTH POLICY MANAGER")
+    print("CRED FILE", service_acct_cred_file)
+    return AzureAuthorizationPolicyManager(
+        service_acct_cred_file, capability_enc_key_file
+    )
+
+
+def check_request_from_policy(public_key, request) -> Tuple[bool, Union[str, None]]:
+    """
+    Check the request using the predefined policy manager.
+
+    Returns a tuple
+    """
+    # TODO: don't hard code public key
+    public_key = "skypilot_eval"
+    logger = get_logger()
+    print_and_log(
+        logger, f"Check request public key: {public_key} (request: {request})"
+    )
+
+    policy_manager = get_policy_manager()
+    authorization_policy_manager = get_authorization_policy_manager()
+    policy = policy_manager.get_policy(public_key, None)
+    print_and_log(logger, f"Got policy {policy}")
+    policy.set_authorization_manager(authorization_policy_manager)
+
+    valid = policy.check_request(request)
+    if not valid:
+        return (False, None)
+    # Check if a service account should be attached to the VM
+    if policy.valid_authorization:
+        return (True, policy.valid_authorization)
+    # If no service account should be attached, return True
+    print(">>> CHECK REQUEST: No service account should be attached")
+    return (True, None)
---
+++ azure-client-proxy/proxy_util/skypilot_forward.py
@@ -0,0 +1,294 @@
+"""
+Forwarding for SkyPilot requests.
+"""
+
+import json
+import os
+from collections import namedtuple
+from http import HTTPStatus
+from urllib.parse import urlparse
+
+import requests
+from flask import Flask, Response, request
+
+from skydentity.policies.checker.azure_authorization_policy import AzureAuthorizationPolicy
+
+from .credentials import (
+    activate_service_account,
+    get_service_account_auth_token,
+    get_service_account_path,
+)
+from .logging import get_logger, print_and_log
+from .policy_check import check_request_from_policy, get_authorization_policy_manager
+from .signature import strip_signature_headers, verify_request_signature
+
+# global constants
+COMPUTE_API_ENDPOINT = os.environ.get(
+    "COMPUTE_API_ENDPOINT", "https://management.azure.com/"
+)
+
+SkypilotRoute = namedtuple(
+    "SkypilotRoute",
+    [
+        "methods",  # HTTP methods for the route
+        "path",  # Flask rule for the route path
+        "fields",  # fields in the routing rule
+        "view_func",  # explicit view function to use; optional
+    ],
+    defaults=[None, None],  # defaults for "fields" and "view_func"
+)
+
+
+# list of all routes required; must be defined after `build_generic_forward`
+ROUTES: list[SkypilotRoute] = [
+    SkypilotRoute(
+        methods=["GET"],
+        path="/subscriptions/<subscriptionId>/providers/Microsoft.Compute/virtualMachines",
+        fields=["subscriptionId"],
+    ),
+    SkypilotRoute(
+        methods=["PUT"],
+        path="/subscriptions/<subscriptionId>/resourcegroups/<resourceGroupName>",
+        fields=["subscriptionId", "resourceGroupName"],
+    ),
+    SkypilotRoute(
+        methods=["GET"],
+        path="/subscriptions/<subscriptionId>/resourceGroups/<resourceGroupName>/providers/Microsoft.Network/networkInterfaces/<nicName>",
+        fields=["subscriptionId", "resourceGroupName", "nicName"],
+    ),
+    SkypilotRoute(
+        methods=["GET", "PUT"],
+        path="/subscriptions/<subscriptionId>/resourceGroups/<resourceGroupName>/providers/Microsoft.Network/publicIpAddresses/<ipName>",
+        fields=["subscriptionId", "resourceGroupName", "ipName"],
+    ),
+    SkypilotRoute(
+        methods=["GET"],
+        path="/subscriptions/<subscriptionId>/providers/Microsoft.Compute/locations/<region>/operations/<operationId>",
+        fields=["subscriptionId", "region", "operationId"],
+    ),
+    SkypilotRoute(
+        methods=["GET", "PUT"],
+        path="/subscriptions/<subscriptionId>/resourceGroups/<resourceGroupName>/providers/Microsoft.Network/virtualNetworks/<virtualNetworkName>",
+        fields=["subscriptionId", "resourceGroupName", "virtualNetworkName"],
+    ),
+    SkypilotRoute(
+        methods=["GET", "PUT"],
+        path="/subscriptions/<subscriptionId>/resourceGroups/<resourceGroupName>/providers/Microsoft.Network/virtualNetworks/<virtualNetworkName>/subnets/<subnetName>",
+        fields=["subscriptionId", "resourceGroupName", "virtualNetworkName", "subnetName"],
+    ),
+    # skydentity internal route
+    SkypilotRoute(
+        methods=["POST"],
+        path="/skydentity/cloud/<cloud>/create-authorization",
+        fields=["cloud"],
+        # wrapper to allow for the function to be defined later
+        view_func=lambda cloud: create_authorization_route(cloud),
+    ),
+]
+
+
+def generic_forward_request(request, log_dict=None):
+    """
+    [SkyPilot Integration]
+    Forward a generic request to Azure APIs.
+    """
+    logger = get_logger()
+    if log_dict is not None:
+        log_str = f"PATH: {request.full_path}\n"
+        for key, val in log_dict.items():
+            log_str += f"\t{key}: {val}\n"
+        print_and_log(logger, log_str.strip())
+
+    if not verify_request_signature(request):
+        # Not sure if this is the correct status code. This case will only happen when the timestamp
+        # associated with the signature is too old at the time when the signature is verified.
+        return Response("", HTTPStatus.REQUEST_TIMEOUT, {})
+
+    # Check the request
+    authorized, service_account_id = check_request_from_policy(request.headers["X-PublicKey"], request)
+    if not authorized:
+        print_and_log(logger, "Request is unauthorized")
+        return Response("Unauthorized", 401)
+
+    # Get new endpoint and new headers
+    new_url = get_new_url(request)
+    new_headers = get_headers_with_auth(request)
+
+    # don't modify the body in any way
+    new_json = None
+    if len(request.get_data()) > 0:
+        new_json = request.json
+        if service_account_id:
+            # TODO(kdharmarajan): replace this with what works for Azure
+            new_json = get_json_with_service_account(request, service_account_id)
+            print_and_log(logger, f"Json with service account: {new_json}")
+
+    azure_response = send_azure_request(request, new_headers, new_url, new_json=new_json)
+    return Response(azure_response.content, azure_response.status_code, new_headers)
+
+
+def build_generic_forward(path: str, fields: list[str]):
+    """
+    Return the appropriate generic forward view function for the fields provided.
+
+    The path is only used to create a unique and readable name for the anonymous function.
+    """
+    # TODO(kdharmarajan): Fix the fields here
+    func = None
+    if fields == ["project"]:
+        func = lambda project: generic_forward_request(request, {"project": project})
+    elif fields == ["project", "region"]:
+        func = lambda project, region: generic_forward_request(
+            request, {"project": project, "region": region}
+        )
+    elif fields == ["project", "family"]:
+        func = lambda project, family: generic_forward_request(
+            request, {"project": project, "family": family}
+        )
+    elif fields == ["project", "network"]:
+        func = lambda project, network: generic_forward_request(
+            request, {"project": project, "network": network}
+        )
+    elif fields == ["project", "operation"]:
+        func = lambda project, operation: generic_forward_request(
+            request, {"project": project, "operation": operation}
+        )
+    elif fields == ["project", "region", "operation"]:
+        func = lambda project, region, operation: generic_forward_request(
+            request, {"project": project, "region": region, "operation": operation}
+        )
+    elif fields == ["project", "region", "instance"]:
+        func = lambda project, region, instance: generic_forward_request(
+            request, {"project": project, "region": region, "instance": instance}
+        )
+    else:
+        raise ValueError(
+            f"Invalid list of variables to build generic forward for: {fields}"
+        )
+
+    # Flask expects all view functions to have unique names
+    # (otherwise it complains about overriding view functions)
+    func.__name__ = path
+
+    return func
+
+
+def setup_routes(app: Flask):
+    """
+    Set up all routes for SkyPilot forwarding.
+    """
+    for route in ROUTES:
+        if route.view_func is not None:
+            # use view_func if specified; change name to be consistent with generic
+            # (also allows lambda functions to be passed in)
+            route.view_func.__name__ = route.path
+            app.add_url_rule(
+                route.path, view_func=route.view_func, methods=route.methods
+            )
+        elif route.fields is not None:
+            # create a generic view function from the fields
+            app.add_url_rule(
+                route.path,
+                view_func=build_generic_forward(route.path, route.fields),
+                methods=route.methods,
+            )
+        else:
+            raise ValueError(
+                "Invalid route specification; missing either `view_func` or `fields`"
+            )
+
+
+def get_json_with_service_account(request, service_account_email):
+    """
+    Modify the JSON of the request to include service account details.
+    """
+    json_dict = request.json
+    service_account_dict = {
+        "email": f"{service_account_email}",
+        "scopes": [f"https://www.Azureapis.com/auth/cloud-platform"],
+    }
+    new_dict = json_dict.copy()
+    new_dict["serviceAccounts"] = [service_account_dict]
+    return new_dict
+
+
+def get_headers_with_auth(request):
+    """
+    Append authentication headers for a service account to the request.
+    """
+    # print_and_log(logger, "Entered get_headers_with_auth")
+    ## Get authorization token and add to headers
+    new_headers = {k: v for k, v in request.headers}  # if k.lower() == 'host'}
+
+    # print_and_log(logger, f"ORIGINAL HEADERS: {new_headers}")
+    parsed_compute_api_endpoint = urlparse(f"{COMPUTE_API_ENDPOINT}")
+    hostname = parsed_compute_api_endpoint.netloc
+    new_headers["Host"] = f"{hostname}"
+
+    # Activate service account and get auth token
+    service_acct_cred_file = get_service_account_path()
+    activate_service_account(service_acct_cred_file)
+
+    auth_token_process_out_bytes = get_service_account_auth_token()
+
+    auth_token = auth_token_process_out_bytes.strip().decode("utf-8")
+    # print_and_log(logger, f"AUTH TOKEN: {auth_token}")
+    new_headers["Authorization"] = f"Bearer {auth_token}"
+
+    clean_new_headers = strip_signature_headers(new_headers)
+    return clean_new_headers
+
+
+def get_new_url(request):
+    """
+    Redirect the URL (originally to the proxy) to the correct Azure endpoint.
+    """
+    new_url = request.url.replace(request.host_url, f"{COMPUTE_API_ENDPOINT}")
+    logger = get_logger()
+    print_and_log(logger, f"\tNew URL: {new_url}")
+    return new_url
+
+
+def send_azure_request(request, new_headers, new_url, new_json=None):
+    """
+    Send a request to the Azure endpoint, with new headers, URL, and request body.
+    """
+    # If no JSON body, don't include a json body in proxied request
+    if len(request.get_data()) == 0:
+        return requests.request(
+            method=request.method,
+            url=new_url,
+            headers=new_headers,
+            cookies=request.cookies,
+            allow_redirects=False,
+        )
+    return requests.request(
+        method=request.method,
+        url=new_url,
+        headers=new_headers,
+        json=new_json,
+        cookies=request.cookies,
+        allow_redirects=False,
+    )
+
+
+def create_authorization_route(cloud):
+    logger = get_logger()
+    authorization_policy_manager = get_authorization_policy_manager()
+    print_and_log(logger, f"Creating authorization (json: {request.json})")
+    # TODO: change hard coded name
+    request_auth_dict = authorization_policy_manager.get_policy_dict("skypilot_eval")
+    print("Request auth dict:", request_auth_dict)
+    authorization_policy = AzureAuthorizationPolicy(policy_dict=request_auth_dict)
+    authorization_request, success = authorization_policy.check_request(request)
+    if success:
+        service_account_id = (
+            authorization_policy_manager.create_service_account_with_roles(
+                authorization_request
+            )
+        )
+        capability_dict = authorization_policy_manager.generate_capability(
+            service_account_id
+        )
+        return Response(json.dumps(capability_dict), 200)
+    return Response("Unauthorized", 401)
