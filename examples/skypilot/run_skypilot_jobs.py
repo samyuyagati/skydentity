4576ab6c835a6038b79b9816588521e7b5d9bded
Karthik Dharmarajan <karthikdharmarajan@berkeley.edu>
2024-04-11 16:08:59 -0700

    Added more key files

---
+++ examples/skypilot/run_skypilot_jobs.py
@@ -0,0 +1,159 @@
+import time
+import os
+import argparse
+import os
+import subprocess
+import time
+
+parser = argparse.ArgumentParser(
+    prog="RunSkyPilotJobs", description="Launches SkyPilot jobs"
+)
+
+parser.add_argument(
+    "--num-jobs", type=int, default=40, help="Number of instances to launch"
+)
+parser.add_argument(
+    "--batch-size", type=int, default=10, help="Requests per batch"
+)
+parser.add_argument(
+    "--with-skydentity", action="store_true", help="Whether to use Skydentity"
+)
+parser.add_argument(
+    "--cloud", type=str, default="gcp", help="Cloud to use"
+)
+parser.add_argument(
+    "--ca-bundle", type=str, default=None, help="Path to CA bundle"
+)
+args = parser.parse_args()
+
+job = """\
+resources:
+  instance_type: n1-standard-1
+  cloud: gcp
+  zone: us-west1-b
+  image_id: projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240223
+
+run: |
+  secs=$(python -S -c 'import random; random.seed({i}); print(random.randint(20*60, 30*60))')
+
+  echo Job duration $secs seconds.
+  sleep $secs
+  echo Job done.
+"""
+
+if args.cloud == "azure":
+    job = """\
+resources:
+  instance_type: Standard_B1ms
+  cloud: azure
+  region: westus
+
+run: |
+  secs=$(python -S -c 'import random; random.seed({i}); print(random.randint(20*60, 30*60))')
+
+  echo Job duration $secs seconds.
+  sleep $secs
+  echo Job done.
+"""
+
+
+# NOTE: if concurrent experiments - change name!
+prefix = "skydentity-test"
+
+NUM_JOBS = args.num_jobs
+api_endpoint = "https://compute.googleapis.com/"
+if args.cloud == "azure":
+    api_endpoint = "https://management.azure.com/"
+    if args.with_skydentity:
+        api_endpoint = "https://127.0.0.1:5000/"
+# GCP with skydentity
+if args.with_skydentity:
+    api_endpoint = "http://127.0.0.1:5000/"
+
+print("API endpoint:", api_endpoint)
+
+start_time = time.perf_counter()
+
+jobs_per_batch = args.batch_size
+num_batches = NUM_JOBS // jobs_per_batch if NUM_JOBS % jobs_per_batch == 0 else (NUM_JOBS // jobs_per_batch) + 1
+
+i = 0
+for batch in range(num_batches):
+    batch_start = time.perf_counter()
+    subprocesses: list[subprocess.Popen] = []
+    fds = []
+    if (NUM_JOBS - i) < jobs_per_batch:
+        jobs_per_batch = NUM_JOBS - i
+    for j in range(jobs_per_batch):
+        with open("job.yaml", "w") as f:
+            f.write(job.format(i=i))
+
+        print(f"Launching job {i}")
+
+        out_fd = open(f"skypilot_benchmark_stdout_{i}.txt", "wb")
+        err_fd = open(f"skypilot_benchmark_stderr_{i}.txt", "wb")
+        fds.append(out_fd)
+        fds.append(err_fd)
+
+        # Paper used subprocess.Run (fully sequential, waits for completion)
+        if args.ca_bundle is not None:
+            cur_subprocess = subprocess.Popen(
+                [
+                    "time",
+                    "sky",
+                    "launch",
+                    "--retry-until-up",
+                    "-y",
+                    "-d",
+                    "-n",
+                    f"{prefix}-{i}",
+                    "job.yaml",
+                    "-c",
+                    "skydentity"
+                ],
+                stdout=out_fd,
+                stderr=err_fd,
+                env={
+                    **os.environ,
+                    "CLOUDSDK_API_ENDPOINT_OVERRIDES_COMPUTE": f"{api_endpoint}",
+                    "REQUESTS_CA_BUNDLE": f"{args.ca_bundle}"
+                },
+            )
+        else:
+            cur_subprocess = subprocess.Popen(
+                [
+                    "time",
+                    "sky",
+                    "launch",
+                    "--retry-until-up",
+                    "-y",
+                    "-d",
+                    "-n",
+                    f"{prefix}-{i}",
+                    "job.yaml",
+                    "-c",
+                    "skydentity"
+                ],
+                stdout=out_fd,
+                stderr=err_fd,
+                env={
+                    **os.environ,
+                    "CLOUDSDK_API_ENDPOINT_OVERRIDES_COMPUTE": f"{api_endpoint}"
+                },
+            )
+        subprocesses.append(cur_subprocess)
+        i += 1
+
+    # wait for all subprocesses to complete
+    for proc in subprocesses:
+        proc.wait()
+
+    batch_end = time.perf_counter()
+
+    # close all file descriptors
+    for fd in fds:
+        fd.flush()
+        fd.close()
+
+    print("BATCH TIME (s):", batch_end - batch_start)
+print("TOTAL TIME (s):", time.perf_counter() - start_time)
---
+++ examples/skypilot/setup_skypilot_benchmark.sh
@@ -0,0 +1,18 @@
+# Usage: ./setup_skypilot_benchmark.sh <path-to-firebase-creds>
+
+mkdir -p keys
+if ! [ -f keys/public.pem ]; then
+  python gen_key_pair.py
+fi
+
+FIREBASE_CREDS=$1
+
+ROOT=$(dirname $(dirname $(dirname $(pwd))))
+echo "Root: $ROOT"
+# Setup authorizations and start servers
+echo "Setting up authorizations..."
+./start_proxies_and_setup_auth.sh -r $ROOT \
+                         -p "sky-identity" \
+                         -k $ROOT/skydentity/examples/skypilot/keys/public.pem \
+                         -s $ROOT/skydentity/examples/skypilot/keys/private.pem \
+                         -c $FIREBASE_CREDS -y $ROOT/skydentity/examples/skypilot/config 
