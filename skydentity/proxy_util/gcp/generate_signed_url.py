0940176ab85842fafa9bb0e1828e5197c6cb3739
samyuyagati <samyuyagati@users.noreply.github.com>
2025-06-20 11:10:07 -0700

    Update generate_signed_url.py
    Add download method

--- skydentity/proxy_util/gcp/generate_signed_url.py
+++ skydentity/proxy_util/gcp/generate_signed_url.py
@@ -7,7 +7,7 @@ def get_credentials_from_service_account_key(key_file: str) -> service_account.C
     return service_account.Credentials.from_service_account_file(
         key_file)
 
-
+# TODO: Distinguish between upload and overwrite -- can we do that w/ signed URLs?
 def generate_upload_signed_url_v4(bucket_name: str, blob_name: str, key_file: str):
     """Generates a v4 signed URL for uploading a blob using HTTP PUT.
 
@@ -36,6 +36,34 @@ def generate_upload_signed_url_v4(bucket_name: str, blob_name: str, key_file: st
         "curl -X PUT -H 'Content-Type: application/octet-stream' "
         "--upload-file my-file '{}'".format(url)
     )
+    return url
+
+def generate_download_signed_url_v4(bucket_name, blob_name):
+    """Generates a v4 signed URL for downloading a blob.
+
+    Note that this method requires a service account key file. You can not use
+    this if you are using Application Default Credentials from Google Compute
+    Engine or from the Google Cloud SDK.
+    """
+    # bucket_name = 'your-bucket-name'
+    # blob_name = 'your-object-name'
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+
+    url = blob.generate_signed_url(
+        version="v4",
+        # This URL is valid for 15 minutes
+        expiration=datetime.timedelta(minutes=15),
+        # Allow GET requests using this URL.
+        method="GET",
+    )
+
+    print("Generated GET signed URL:")
+    print(url)
+    print("You can use this URL with any user agent, for example:")
+    print(f"curl '{url}'")
     return url
 
 generate_upload_signed_url_v4("test_bucket_signedurl", "blob1", "/Users/samyu/.cloud_creds/gcp/sky-identity-ac2febc1b9b3.json")
